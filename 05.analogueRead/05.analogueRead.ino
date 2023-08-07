6e1569112c51cfd9ce5372bb83efcf4f3ae168f7
Jaiden Wu <jaiden.wu@education.nsw.gov.au>
2023-08-07 12:36:26 +1000

    completed activity 5

--- 05.analogueRead/05.analogueRead.ino
+++ 05.analogueRead/05.analogueRead.ino
@@ -9,17 +9,43 @@
     5. I can apply this knowledge to the Light, Sound and Rotary Potentiometer in the sensor kit
 
   Student Notes: 
-
+Range of analogue data: 0-1023
   Documentation: 
     https://www.arduino.cc/reference/en/language/functions/analog-io/analogread/
   Schematic:
     https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/05.serialRead/Bootcamp-serialRead.png
 */
 
-void setup() {
+static unsigned int myVarResistor = A0;
+static unsigned int mySound = A2;
+static unsigned int myLight = A3;
 
+
+
+void setup() {
+Serial.begin(9600);
+Serial.println("Serial Monitor Configured to 9600");
+Serial.println("-----------------");
 }
 
 void loop() {
+unsigned int val = analogRead(myVarResistor);
+Serial.print("PotentiometerValue:");
+Serial.print (val);
+Serial.print (",");
+
+unsigned int senVal = analogRead(mySound);
+Serial.print("mySoundValue:");
+Serial.print (senVal);
+Serial.print (",");
+
+unsigned int benVal = analogRead(myLight);
+Serial.print("myLightValue:");
+Serial.print (benVal);
+Serial.print (",");
+
+Serial.println (); 
+
+
 
 }
