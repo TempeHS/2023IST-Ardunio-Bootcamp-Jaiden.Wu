58f9171b29e8caf0dddd23388f503ff7e23ddcc1
Jaiden Wu <jaiden.wu@education.nsw.gov.au>
2023-08-23 14:39:19 +1000

    Catchup for absence, completed 8&9
    In my absence, I have looked over the code and notes and understood it.

--- 08.binaryLogic/08.binaryLogic.ino
+++ 08.binaryLogic/08.binaryLogic.ino
@@ -1,5 +1,5 @@
 /*
-  Author: 
+  Author: Jaiden Wu
    Learning Intention: The students understand binary logic and apply it to a range of control structures.
     1. I understand Binary Logic is asking questions that only have a true and false outcome
     2. I can apply different comparison operators to get a desired outcome in a basic if else control structure 
@@ -30,11 +30,22 @@
   Schematic: 
     
 */
+unsigned static int myLED = 13;
+
 
 void setup() {
-
+Serial.begin(9600);
+Serial.println("Serial Monitor Configured to 9600");
+Serial.println("-------------------------");
+pinMode(myLED, OUTPUT);
 }
 
 void loop() {
-
+for(int i = 0; i < 5; i++){
+    analogWrite(myLED, HIGH);
+    delay(1000);
+    analogWrite(myLED, LOW);
+    delay(1000);
+    Serial.println(i);
+}
 }
--- 09.workingWithTime/09.workingWithTime.ino
+++ 09.workingWithTime/09.workingWithTime.ino
@@ -1,5 +1,5 @@
 /*
-  Author: 
+  Author: Jaiden Wu
    Learning Intention: The students understand how time is used in microcontrollers and best practices
     1. I understand why delay(1000) whilst easy is not the preferred way of time event management
     2. I can use the millis() method and binary logic to trigger events by time
@@ -7,6 +7,7 @@
     4. I understand how time can be used to send a communication frequency by using micros()
 
   Student Notes: 
+- millis() = Returns the number of milliseconds passed since the Arduino board began running the current program.
 
   Documentation: 
     https://www.arduino.cc/reference/en/#functions
@@ -15,10 +16,29 @@
 */
 
 
-void setup() {
+// LED and bool to turn led on or off
+static unsigned int myLED = 13;
+bool myLedStatus = false;
 
+// both as longs so you can mathematically compare
+unsigned long myLEDPrevMillis = 0;
+const unsigned long myLedInterval = 2000;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Serial Monitor Configured to 9600");
+  Serial.println("-----------------------");
+  pinMode(myLED, OUTPUT);
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
+ digitalWrite(myLED, myLedStatus);
+ Serial.print(currentMillis);
+ Serial.print(",");
+ Serial.println(myLedStatus);
  
-}
+ if(currentMillis - myLEDPrevMillis >= myLedInterval){
+    myLedStatus = !myLedStatus;
+    myLEDPrevMillis = currentMillis;
+ }
